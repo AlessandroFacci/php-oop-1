1df909b7f2e04d9672e63dcf00f112cc30b45843
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-09-29 16:17:29 +0200

    db.php: create objects array

---
+++  objects_array/db.php
@@ -0,0 +1,31 @@
+<?php
+
+$avengers1 = new Movie (
+"The Avengers",
+"Action",
+"2h 23m",
+"English"
+);
+
+$avengers2 = new Movie (
+  "Avengers: Age of Ultron",
+  "Action",
+  "2h 21m",
+  "English"
+);
+
+$avengers3 = new Movie (
+  "Avengers: Infinity War",
+  "Action",
+  "2h 29m",
+  "English"
+);
+
+$avengers4 = new Movie (
+  "Avengers: Endgame",
+  "Action",
+  "3h 2m",
+  "English"
+);
+
+?>
