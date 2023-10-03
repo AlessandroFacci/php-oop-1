03b22c7fa9733b644545b8a719433e8449cd26d1
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-03 09:49:11 +0200

    index.php: import bootstrap

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ require_once __DIR__ . '/models/production.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <title>Document</title>
 </head>
 
